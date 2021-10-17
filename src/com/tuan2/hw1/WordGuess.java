d747c2e9d973e5a3da73985fbc46280e9a9d9247
KomaHomu <ssjggoku96@gmail.com>
2021-10-17 17:35:07 +0700

    update

--- src/com/tuan2/hw1/WordGuess.java
+++ src/com/tuan2/hw1/WordGuess.java
@@ -3,8 +3,10 @@ package com.tuan2.hw1;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class WordGuess {
 
@@ -29,13 +31,60 @@ public class WordGuess {
 
     public static void guessingGame() {
 
-        final String SECRET_WORD = args[0]; // set as args[0]
+        Scanner scan = new Scanner(System.in);
 
+        final String SECRET_WORD = getSecretWord(); // set as args[0] if you want to get the word using command-line
+
+        String guess;
+        int turn = 1;
         StringBuilder temp;
+        char test;
         char[] arr = new char[SECRET_WORD.length()];
-        int count = 0;
+
+        Arrays.fill(arr, '-');
+        temp = new StringBuilder(new String(arr));
 
         System.out.println("The secret word is: " + SECRET_WORD);
+
+        System.out.print("Key in one character or your guess word: ");
+        guess = scan.next();
+
+        while (true) {
+            if (guess.length() == SECRET_WORD.length()) {
+                if (guess.equals(SECRET_WORD)) {
+                    break;
+                } else {
+                    System.out.println("Incorrect word.");
+                }
+            }
+
+            if (guess.length() == 1) {
+                test = guess.charAt(0);
+                for (int i = 0; i < SECRET_WORD.length(); i++) {
+                    if (test == SECRET_WORD.charAt(i)) {
+
+                        temp.setCharAt(i, test);
+                    }
+                }
+
+                if (temp.toString().equals(SECRET_WORD)) {
+                    break;
+                }
+            }
+            System.out.println("Trial " + turn + ": " + temp);
+
+            turn++;
+
+            System.out.print("Key in one character or your guess word: ");
+            guess = scan.next();
+        }
+
+        System.out.println("Congratulation!");
+        if (turn == 1) {
+            System.out.println("You got it in 1 trial!");
+        } else {
+            System.out.println("You got it in " + turn + " trials!");
+        }
     }
 
     public static void main(String[] args) {
